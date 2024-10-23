98ad7d77eb716e55f0a4cb8d9a9cbf3d5349587c
Boyuan Jiang <jiangby03@sina.com>
2024-10-23 16:59:24 +0100

    clipboard config

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -1,12 +1,10 @@
-local t = vim.g.neovide and true or false
-
 return {
   {
     "onedarkpro.nvim",
     opts = {
       options = {
         cursorline = true,
-        transparency = not t,
+        transparency = true,
       },
     },
   },
